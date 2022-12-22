36e1a48e517b3ba7ab6005510458ef98efcdb747
simionroxanavioleta <simionroxanavioleta@gmail.com>
2022-12-22 19:23:23 +0200

    Lab3

--- Pages/Books/Index.cshtml.cs
+++ Pages/Books/Index.cshtml.cs
@@ -8,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using Simion_Roxana_Lab2.Data;
 using Simion_Roxana_Lab2.Models;
 
+
 namespace Simion_Roxana_Lab2.Pages.Books
 {
     public class IndexModel : PageModel
@@ -19,7 +20,29 @@ namespace Simion_Roxana_Lab2.Pages.Books
             _context = context;
         }
 
-        public IList<Book> Book { get;set; } = default!;
+        public IList<Book> Book { get;set; }
+        public BookData BookD { get; set; }
+        public int BookID { get; set; }
+        public int CategoryID { get; set; }
+        public async Task OnGetAsync(int? id, int? categoryID)
+        {
+            BookD = new BookData();
+
+            BookD.Books = await _context.Book
+            .Include(b => b.Publisher)
+            .Include(b => b.BookCategories)
+            .ThenInclude(b => b.Category)
+            .AsNoTracking()
+            .OrderBy(b => b.Title)
+            .ToListAsync();
+            if (id != null)
+            {
+                BookID = id.Value;
+                Book book = BookD.Books
+                .Where(i => i.ID == id.Value).Single();
+                BookD.Categories = book.BookCategories.Select(s => s.Category);
+            }
+        }
 
         public async Task OnGetAsync()
         {
